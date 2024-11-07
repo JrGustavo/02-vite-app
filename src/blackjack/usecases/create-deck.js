66318822e3d3bd197921eedd22b47a5a6f80255f
JrGustavo <gus@soypetal.com>
2024-11-07 18:18:06 -0500

    Update Black Jack

--- src/blackjack/usecases/create-deck.js
+++ src/blackjack/usecases/create-deck.js
@@ -1,9 +1,22 @@
 import _ from 'underscore'
 
 
+/**
+ * Esta función crea un nuevo deck
+ * @param {Array<String>} tiposDeCarta Ejemplo: ['C','D','H','S']
+ * @param {Array<String>} tiposEspeciales Ejemplo: ['A','J','Q','K']
+ * @returns {Array<String>} retorna un nuevo deck de cartas
+ */
 
-// Esta función crea un nuevo deck
+
 export const crearDeck = (tiposDeCarta, tiposEspeciales) => {
+
+    if( !tiposDeCarta  || tiposDeCarta.length === 0 )
+         throw new Error('tiposDeCarta es obligatorio')
+
+    if( !tiposEspeciales  || tiposEspeciales.length === 0 )
+        throw new Error('tiposDeCarta tiene que ser un arreglo string')
+
 
     let deck = []
 
@@ -12,6 +25,7 @@ export const crearDeck = (tiposDeCarta, tiposEspeciales) => {
             deck.push( i + tipo);
         }
     }
+
 
     for( let tipo of tiposDeCarta ) {
         for( let esp of tiposEspeciales ) {
